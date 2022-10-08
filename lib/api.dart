eda7a9bd57e91b96358a9f108a8153f5ac27c400
Naveen Kumar <naveen.kumar@oodles.io>
2022-10-08 20:03:18 +0530

    Initial Inherited Commit

---
+++ lib/api.dart
@@ -0,0 +1,14 @@
+class Api {
+  String? dateAndTime;
+
+  //return dateAnd time and assign dateAndTime in variable;
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:contack_book/api.dart';
+import 'package:contack_book/api_provider.dart';
+import 'package:contack_book/date_time_widget.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ApiProvider(
+        api: Api(),
+        child: const ContactsList(),
+      ),
+    );
+  }
+}
+
+class ContactsList extends StatefulWidget {
+  const ContactsList({super.key});
+
+  @override
+  State<ContactsList> createState() => _ContactsListState();
+}
+
+class _ContactsListState extends State<ContactsList> {
+  ValueKey _textKey = const ValueKey<String?>(null);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text(ApiProvider.of(context).api.dateAndTime ?? "Inherited Widget"),
+      ),
+      body: GestureDetector(
+        onTap: () async {
+          final api = ApiProvider.of(context).api;
+          final dateAndTime = await api.getDateAndTime();
+          setState(() {
+            _textKey = ValueKey(dateAndTime);
+          });
+        },
+        child: SizedBox.expand(
+          child: Container(
+            color: Colors.white,
+            child: DateTimeWidget(key: _textKey),
+          ),
+        ),
+      ),
+    );
+  }
+}
