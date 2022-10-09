95bc0904614b2e7d93f557ee10add4ed4ef7c476
Naveen Kumar <naveen.kumar@oodles.io>
2022-10-09 12:53:42 +0530

    code clean

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Inherited Model',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ApiProvider(
-        api: Api(),
-        child: const ContactsList(),
-      ),
+      home: const ContactsList(),
     );
   }
 }
@@ -36,32 +33,15 @@ class ContactsList extends StatefulWidget {
 }
 
 class _ContactsListState extends State<ContactsList> {
-  ValueKey _textKey = const ValueKey<String?>(null);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:
-            Text(ApiProvider.of(context).api.dateAndTime ?? "Inherited Widget"),
+            Text("Inherited Model"),
       ),
-      body: GestureDetector(
-        onTap: () async {
-          final api = ApiProvider.of(context).api;
-          final dateAndTime = await api.getDateAndTime();
-          setState(() {
-            _textKey = ValueKey(dateAndTime);
-          });
-        },
-        child: SizedBox.expand(
-          child: Container(
-            color: Colors.white,
-
-            ///this is optional we can either use this widget
-            ///or we can use ApiProvider.of(context).api.dateAndTime??""
-            child: DateTimeWidget(key: _textKey),
-          ),
-        ),
-      ),
+      body: Container(),
     );
   }
 }
